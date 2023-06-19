082d045eecac2635fe1884d1f7742ef40a6b721c
encodedbrain <marcodamascenodev@gmail.com>
2023-06-19 14:07:57 -0300

    :wrench: chore: added services,database context added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using FinSharp.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<LocalDb>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
