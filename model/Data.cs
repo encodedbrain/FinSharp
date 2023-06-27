bba6934268b7341e2c46b47611138de98f304fea
encodedbrain <marcodamascenodev@gmail.com>
2023-06-27 17:40:25 -0300

    :construction: added builder

--- model/Data.cs
+++ model/Data.cs
@@ -1,21 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace FinSharp.model
 {
     public class CPF
     {
+        public CPF(string? email, string? password, string? cpf, string? phone)
+        {
+            Email = email;
+            Password = password;
+            Cpf = cpf;
+            Phone = phone;
+        }
+
         public string? Email { get; private set; }
 
-        // [Required]
         public string? Password { get; private set; }
 
-        // [Required]
         public string? Cpf { get; private set; }
 
-        // [Required]
         public string? Phone { get; private set; }
     }
 }
