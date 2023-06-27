8c1ba29af59e8be0214fda544869288371a31c2d
encodedbrain <marcodamascenodev@gmail.com>
2023-06-26 23:57:43 -0300

    :recycle: refactor: added schemas

---
+++ model/Profile.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinSharp.model
+{
+    public class Profile
+    {
+
+        public string? Nickname { get; private set; }
+
+
+        public int Age { get; private set; }
+
+
+        public string? Photo { get; private set; }
+    }
+}
